2f3f332402293b9b1fc85dacb7555f75d831c13e
nana062877 <131091948+nana062877@users.noreply.github.com>
2023-10-17 10:39:11 +0900

    Create ex6-2.js

---
+++ ex6-2.js
@@ -0,0 +1,10 @@
+var who66 = 'world';
+function sayhello66(){
+  alert('Hello,' + who66 + '!');
+}
+function taro66(){
+  alert(who66 = '太郎' + 'が来た';)
+}
+function hanako66(){
+  alert(who66 = '花子' + 'が来た';)
+}
