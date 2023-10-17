27b5663fdc1ae1cdcc0b8b0b5d2301ec25b77f20
nana062877 <131091948+nana062877@users.noreply.github.com>
2023-10-17 10:43:19 +0900

    Update ex6-2.js

--- ex6-2.js
+++ ex6-2.js
@@ -3,8 +3,8 @@ function sayhello66(){
   alert('Hello,' + who66 + '!');
 }
 function taro66(){
-  alert(who66 = '太郎' + 'が来た';)
+  alert(who66 = '太郎' + 'が来た');
 }
 function hanako66(){
-  alert(who66 = '花子' + 'が来た';)
+  alert(who66 = '花子' + 'が来た');
 }
