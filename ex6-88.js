3b26b270bea7f072931fac243f496fea9f8d6868
nana062877 <131091948+nana062877@users.noreply.github.com>
2023-10-24 10:44:46 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,9 +1,3 @@
-/**
-function gazo688(){
-  document.getElementById('who688').innerHTML = '笑顔にする';
-}
-**/
-
 function gazo688()
   {document.getElementById('who688').innerHTML=
   '<img src="./image/nikkori.png" alt="笑顔">'
