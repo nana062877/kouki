99387e1352172778607923189cb052e99b6ba081
nana062877 <131091948+nana062877@users.noreply.github.com>
2023-10-31 10:37:38 +0900

    Create ex06-10-2.js

---
+++ ex06-10-2.js
@@ -0,0 +1,9 @@
+function imgchange3(){
+  document.getElementById('logo').src =
+    './image/sai.jpg';
+}
+
+function imgchange4(){
+  document.getElementById('logo').src =
+    './image/rogo.jpg';
+}
