4617c8e20cc4e2d32d1d214b7bc7acec24d66e18
nana062877 <131091948+nana062877@users.noreply.github.com>
2023-10-31 10:21:08 +0900

    Update ex06-10.js

--- ex06-10.js
+++ ex06-10.js
@@ -2,3 +2,8 @@ function imgchange1(){
   document.getElementById('logo').src =
     './image/sai.jpg';
 }
+
+function imgchange2(){
+  document.getElementById('logo').src =
+    './image/rogo.jpg';
+}
