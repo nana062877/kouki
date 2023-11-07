80d1eed0046bd3034d749b7f63c00472f7ab832e
nana062877 <131091948+nana062877@users.noreply.github.com>
2023-11-07 10:01:08 +0900

    Update ex06-11.js

--- ex06-11.js
+++ ex06-11.js
@@ -2,7 +2,7 @@ function hyoji(){
   //表示する文字
 var str ="一文字ずつ表示します。";
   //テキストボックスの文字数
-var cnt =document.timer.moji.value.length;
+var cnt = document.timer.moji.value.length;
   //文字が全部表示されているか確認
 
 if(cnt < 11){
@@ -10,7 +10,7 @@ if(cnt < 11){
   document.timer.moji.value = str.substr(0,cnt+1);}
   else{
     //全て表示されたら、空文字に戻す
-  document.timer.moji.value="";}}
-function starefnc(){
+  document.timer.moji.value = "";}}
+function startfnc(){
   //関数hyoji()を1000ミリ秒間隔で呼び出す
 setlnterval("hyoji()",1000);}
