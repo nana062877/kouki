40f4827e57a8fc5d0b231999f81799c2f9896916
nana062877 <131091948+nana062877@users.noreply.github.com>
2023-11-07 09:52:30 +0900

    Update ex06-11.js

--- ex06-11.js
+++ ex06-11.js
@@ -5,3 +5,12 @@ var str ="一文字ずつ表示します。";
 var cnt =document.timer.moji.value.length;
   //文字が全部表示されているか確認
 
+if(cnt < 11){
+  //現在よりも1文字多く切り出して表示
+  document.timer.moji.value = str.substr(0,cnt+1);}
+  else{
+    //全て表示されたら、空文字に戻す
+  document.timer.moji.value="";}}
+function starefnc(){
+  //関数hyoji()を1000ミリ秒間隔で呼び出す
+setlnterval("hyoji()",1000);}
