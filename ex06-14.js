65e8e3cfabfbf18e7c6185ca22ed32cd0f4592a1
nana062877 <131091948+nana062877@users.noreply.github.com>
2023-11-21 10:20:39 +0900

    Update ex06-14.js

--- ex06-14.js
+++ ex06-14.js
@@ -2,5 +2,11 @@ function hyoji14(){
  {document.getElementById('who14').innerHTML=
   '<img src="./image/nikkori.png" alt="笑顔">'
   ;}
+
+
+ function hyoji14-2(){
+  document.getElementById('who14').innerHTML=
+    '<img src="./image/syobon.png" alt="悲しい">'
+   ;}
   //setTimeout()を含む関数を呼び込む
-  setTimeout("hyoji14()",1000);}
+  setTimeout("hyoji14-2()",3000);}
