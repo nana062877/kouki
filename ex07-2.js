a4fec3f7dd296fa31ec9001cecc008587306ef4e
nana062877 <131091948+nana062877@users.noreply.github.com>
2023-11-28 10:21:51 +0900

    Update ex07-2.js

--- ex07-2.js
+++ ex07-2.js
@@ -1,5 +1,8 @@
+let number = x;
+let number = y;
+
 function add(){
-  var x = document.getElementById(Number.('input1').value;
-  var y = document.getElementById(Number.('input2').value;
+  var x = document.getElementById('input1').value;
+  var y = document.getElementById('input2').value;
   alert(x+y);
 }
