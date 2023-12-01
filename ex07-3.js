1fcdb2bc2f2855b4841a14d05a4021298457bc7b
nana062877 <131091948+nana062877@users.noreply.github.com>
2023-12-01 11:50:58 +0900

    Update ex07-3.js

--- ex07-3.js
+++ ex07-3.js
@@ -1,6 +1,6 @@
 function BMI(){
   var takasa = Number(document.getElementById('takasa').value);
   var omosa = Number(document.getElementById('omosa').value);
-  var BMI = (omosa / (takasa^2)); //BMIの計算
+  var BMI = omosa / (takasa * takasa); //BMIの計算
   alert('BMI値は' + BMI + 'です。');
 }
