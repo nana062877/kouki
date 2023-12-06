21ef64d670ad62a1f86d3d45771823c0d30dbb3e
nana062877 <131091948+nana062877@users.noreply.github.com>
2023-12-06 12:17:41 +0900

    Update ex07-6.js

--- ex07-6.js
+++ ex07-6.js
@@ -1,8 +1,8 @@
 function nazo3(){
-  if(document.getElementById('answer3').value ≠≠ '夢'){
-    alert('あたり！');
+  if(document.getElementById('answer3').value != '夢'){
+    alert('はずれ');
   }
   else{
-    alert('はずれ');
+    alert('あたり！');
   }
 }
