ca4d9f7d85d6994898bfba5a47468023f6ca7063
nana062877 <131091948+nana062877@users.noreply.github.com>
2023-12-08 11:42:26 +0900

    Update ex07-10.js

--- ex07-10.js
+++ ex07-10.js
@@ -1,7 +1,7 @@
 function BMI2(){
   var takasa2 = Number(document.getElementById('takasa2').value);
   var omosa2 = Number(document.getElementById('omosa2').value);
-  var BMI2 = omosa / (takasa2 * takasa2); //BMIの計算
+  var BMI2 = omosa2 / (takasa2 * takasa2); //BMIの計算
 
   if(BMI2 < 18.5){
   alert('低体重');
