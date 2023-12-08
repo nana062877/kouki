7afc193f1a2af0acdad45a7c5ff6a3f4b2994667
nana062877 <131091948+nana062877@users.noreply.github.com>
2023-12-08 11:27:28 +0900

    Create ex07-10.js

---
+++ ex07-10.js
@@ -0,0 +1,13 @@
+function BMI2(){
+  var takasa2 = Number(document.getElementById('takasa2').value);
+  var omosa2 = Number(document.getElementById('omosa2').value);
+  var BMI2 = omosa / (takasa2 * takasa2); //BMIの計算
+
+  if(document.getElementById('BMI2').value < 18.5){
+  alert('低体重');
+  }else if(document.getElementById('BMI2').value < 25){
+    alert('普通');
+    }else{
+    alert('肥満');
+  }
+}
