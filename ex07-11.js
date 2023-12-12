9bdc08f54ddc8d5a2fd7319437f4bdbad85ba0d4
nana062877 <131091948+nana062877@users.noreply.github.com>
2023-12-12 10:19:21 +0900

    Update ex07-11.js

--- ex07-11.js
+++ ex07-11.js
@@ -1,10 +1,7 @@
 function nenn(){
   var tosi = Number(document.getElementById('tosi').value);
 
-
-if(tosi /= 4 || /= 100 && /= 400){
+if(tosi /= 400){
   alert('閏年です。');
- }else{
-  alert('平年です。');
- }
+}
 }
