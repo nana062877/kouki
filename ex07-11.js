17d1c5d77c85e7f8f995c284d7e9c9d29e72a231
nana062877 <131091948+nana062877@users.noreply.github.com>
2023-12-15 11:05:15 +0900

    Update ex07-11.js

--- ex07-11.js
+++ ex07-11.js
@@ -1,10 +1,27 @@
 function nenn(){
   var tosi = Number(document.getElementById('tosi').value);
 
-if(tosi % 4 ==0 || tosi % 100 != 0 && tosi % 400 ==0){
+if((tosi % 4 ==0 )|| tosi % 100 != 0 && tosi % 400 ==0){
   to = '閏年';
 }else{
   to = '平年';
 }
   alert(tosi + '年は' + to + 'です。')
 }
+
+
+/*
+if(tosi % 4 ==0 || tosi % 100 != 0 && tosi % 400 ==0){
+  to = '閏年';
+}else{
+  to = '平年';
+}
+
+if(tosi % 400){
+  to = '閏年';
+}else if(tosi % 100){
+  to = '平年';
+}else if(tosi % 4){
+  to = '閏年';
+}
+↑どうあがいたって閏年/*
